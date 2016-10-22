f801b68a344613aae5cf3b92166e9af2dc6ffb54
Javier Jimenez Alvarez <javierjimenezalvarez@gmail.com>
2016-10-22 16:03:58 +0200

    Final database schema

---
+++ db_schema/openpet_final.sql
@@ -0,0 +1,123 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.5.52-0ubuntu0.14.04.1)
+# Database: openpet
+# Generation Time: 2016-10-22 16:02:43 +0200
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table locations
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `locations`;
+
+CREATE TABLE `locations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `url` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `phone` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `latitude` decimal(7,4) DEFAULT NULL,
+  `longitude` decimal(7,4) DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Dump of table origins
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `origins`;
+
+CREATE TABLE `origins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `url` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Dump of table races
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `races`;
+
+CREATE TABLE `races` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `specie_id` int(11) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_5DBD1EC9D5436AB7` (`specie_id`),
+  CONSTRAINT `FK_5DBD1EC9D5436AB7` FOREIGN KEY (`specie_id`) REFERENCES `species` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Dump of table species
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `species`;
+
+CREATE TABLE `species` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Dump of table specimens
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `specimens`;
+
+CREATE TABLE `specimens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `origin_id` int(11) DEFAULT NULL,
+  `location_id` int(11) DEFAULT NULL,
+  `race_id` int(11) DEFAULT NULL,
+  `origin_identification` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` longtext COLLATE utf8_unicode_ci,
+  `summary` longtext COLLATE utf8_unicode_ci,
+  `sex` int(11) NOT NULL,
+  `birthdate` date DEFAULT NULL,
+  `entrydate` date DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_F599311E56A273CC` (`origin_id`),
+  KEY `IDX_F599311E64D218E` (`location_id`),
+  KEY `IDX_F599311E6E59D40D` (`race_id`),
+  CONSTRAINT `FK_F599311E6E59D40D` FOREIGN KEY (`race_id`) REFERENCES `races` (`id`),
+  CONSTRAINT `FK_F599311E56A273CC` FOREIGN KEY (`origin_id`) REFERENCES `origins` (`id`),
+  CONSTRAINT `FK_F599311E64D218E` FOREIGN KEY (`location_id`) REFERENCES `locations` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
