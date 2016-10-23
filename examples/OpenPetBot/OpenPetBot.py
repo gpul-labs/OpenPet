0bb03d7fb3e95272ed70ddfb55a5c8f959411c26
Pablo Mariño <pablomarino@users.noreply.github.com>
2016-10-23 13:56:01 +0200

    Actualizado cuerpo del tweet

--- examples/OpenPetBot/OpenPetBot.py
+++ examples/OpenPetBot/OpenPetBot.py
@@ -1,7 +1,8 @@
 from twitter import *
 import time
 import json
-
+import requests
+import random
 
 #t = Twitter(auth=OAuth(accessToken, accessTokenSecret, consumerKey, consumerToken))
 #t.statuses.update(status="OpenPet")
@@ -9,7 +10,7 @@ import json
 def load_config():
     with open('./config.json') as data_file:
         data = json.load(data_file)
-        return {'days':data['delayDays'], 'hours':data['delayHours'], 'minutes':data['delayMinutes'], 'consumerKey':data['consumerKey'], 'consumerToken':data['consumerToken'], 'accessToken':data['accessToken'], 'accessTokenSecret':data['accessTokenSecret']}
+        return {'days':data['delayDays'], 'hours':data['delayHours'], 'minutes':data['delayMinutes'], 'consumerKey':data['consumerKey'], 'consumerToken':data['consumerToken'], 'accessToken':data['accessToken'], 'accessTokenSecret':data['accessTokenSecret'], 'apiUrl':data['apiUrl'], 'recordsPerRequest':data['recordsPerRequest']}
 
 
 def tweet_start():
@@ -17,8 +18,19 @@ def tweet_start():
     t = Twitter(auth=OAuth(cfg['accessToken'], cfg['accessTokenSecret'], cfg['consumerKey'], cfg['consumerToken']))
     # Calculate the tweet interval in minutes
     tweet_interval = (cfg['days']*24*60 + cfg['hours']*60 + cfg['minutes'])*60
-    #while True:
-    t.statuses.update(status="Barrrfff")
-    #time.sleep(tweet_interval)
+
+    # recoger los datos
+    url = cfg['apiUrl']+'?last='+cfg['recordsPerRequest']
+    response = requests.get(url).json()
+
+    randId = response[random.randrange(0,len(response))]['id']
+
+    url = cfg['apiUrl']+'/'+str(randId)
+    response = requests.get(url).json()
+    while True:
+        t.statuses.update("Saluda a "+response['name']+" este "+response['specie']['name']+' busca un hogar, si estas interesado ponte en contacto con '+response['location']['name']+"("+response['location']['url']+")."+response['image'])
+        time.sleep(tweet_interval)
 
 tweet_start()
+
+
